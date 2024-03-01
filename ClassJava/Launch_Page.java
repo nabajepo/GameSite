2e6bffe4b6bdb4308a4ec7275628b88289ebc1ad
nabajepo <nabajepo@gmail.com>
2024-03-01 13:20:47 -0500

    Update Launch_Page.java
    Not necessary package

--- ClassJava/Launch_Page.java
+++ ClassJava/Launch_Page.java
@@ -1,21 +1,6 @@
 
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.StandardCopyOption;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.Timer;
-import java.util.TimerTask;
-
-import javax.swing.JFileChooser;
-
 public class Launch_Page {
     public static void main(String[] args){
        Identification_Page identification=new Identification_Page();
-       
     }
-    
 }
