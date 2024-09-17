6c009e399b65783cb843fbc5c2aafdea2268b407
nabajepo <nabajepo@gmail.com>
2024-09-16 20:10:13 -0400

    Update Launch_Page.java

--- ClassJava/Launch_Page.java
+++ ClassJava/Launch_Page.java
@@ -1,5 +1,7 @@
 public class Launch_Page {
     public static void main(String[] args){
-       Identification_Page identification=new Identification_Page();
+       //Identification_Page identification=new Identification_Page();
+       Tic_Tac_Toe newj=new Tic_Tac_Toe("bahenda", "AvatarImage//Avatar1.PNG");
+       
     }
 }
